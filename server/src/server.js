36a89cc81bce156167df0f61d9fbcd1b736012fa
snehesht <mail@snehesh.me>
2021-02-03 06:10:45 -0500

    Added express server

---
+++ server/src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const logger = require('./lib/logger');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+
+
+let server;
+const startServer = async (port = 8000) => {
+  try {
+    server = app.listen(port, () => logger.info(`Starting api-server on port ${port}`));
+  } catch (error) {
+    logger.error(`Error starting express server, ${error.message}`);
+    logger.errorStack(error);
+    throw error;
+  }
+}
+
+const stopServer = async () => {
+  if (server) {
+    await server.close();
+  }
+}
+
+module.exports = {
+  startServer,
+  stopServer,
+};
