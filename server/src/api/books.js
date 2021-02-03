0da9cc4e76fd1397fd2071df40f4924aa7d9fb9f
snehesht <mail@snehesh.me>
2021-02-03 09:52:13 -0500

    Fixed attributes

--- server/src/api/books.js
+++ server/src/api/books.js
@@ -10,9 +10,19 @@ booksRouter.get('/', async (req, res) => {
     const query = req.query.query || '';
     const limit = parseInt(req.query.limit, 10) || 25;
     const offset = parseInt(req.query.offset, 10) || 0;
-    const books = await Books.search(query, limit, offset)
+    const books = await Books.search(query, limit, offset);
+    const normalizedBooks = [];
+    books.forEach(book => normalizedBooks.push({
+      title: book.title,
+      description: book.description,
+      id: book.id,
+      pages: book.pages,
+      rating: book.rating,
+      authors: book.authors,
+      genres: book.genres,
+    }))
     logger.info(`Querying Books with query '${query}' got ${books.length} results`)
-    return res.send(books)
+    return res.send(normalizedBooks)
   } catch (error) {
     logger.error(`Failed to search books, ${error.message}`);
     return res.status(400).send({ error: 'Failed to search for books' })
